925bf3e3e69f757340047b38802ebf1da2e9d4a5
KarolBogdanskiOfficial <bogdanski@student.agh.edu.pl>
2021-07-14 13:33:08 +0200

    alpine ssh still not working

--- worker2/br_worker2.sh
+++ worker2/br_worker2.sh
@@ -10,4 +10,4 @@ docker stop worker2
 docker rm worker2
 
 docker build -t worker2:${tag} worker2
-docker run --name="worker2" -d -p 2022:2022 --network=${netw} worker2:${tag}
+docker run --name="worker2" -d -p 2022:22 --network=${netw} worker2:${tag}
