23a8cc0a39a1add950e89ec7ba8920f0e39ed453
karolbogdanski <karol.bogdanski3@gmail.com>
2021-07-29 11:28:12 +0200

    replica set issue progressed to authentication problems

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -9,5 +9,5 @@ netw="ansible-swarm"
 docker stop ubuntu
 docker rm ubuntu
 
-docker build -t ubuntu:${tag} ubuntu
-docker run --name="ubuntu" -d -p 2020:22 --network=${netw} ubuntu:${tag}
+#docker build -t ubuntu:${tag} ubuntu
+docker run --name="ubuntu" -d -p 2020:22 -p 27017:27017 --network=${netw} ubuntu:${tag}
